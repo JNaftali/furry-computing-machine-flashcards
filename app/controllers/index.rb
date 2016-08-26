452bb9f259d6f842b97e8328d4affb1a0c099352
Henri El Daher & Joshua Marantz & Melissa Rudofsky <pair+heldaher.jnaftali.mrudof@devbootcamp.com>
2016-08-26 11:46:05 -0400

    Initial commit, splitting up now

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,2 +1,26 @@
 get '/' do
+  erb :index
+end
+
+get '/login' do
+  erb :login
+end
+
+post '/login' do
+  user = User.find_by(username: params[:username])
+
+  #other login stuff
+
+  if new_user.save
+    session[:user_id] = new_user.id
+    redirect '/'
+  else
+    @errors = new_user.errors.full_messages
+    erb :'users/new'
+  end
+end
+
+get '/logout' do
+  session[:user_id] = nil
+  redirect '/'
 end
